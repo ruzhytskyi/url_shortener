2871b4d4b4e1530041b7791bd6d8f519b28c4449
Oleksandr Ruzhytskyi <oruzhytskyi@cogniance.com>
2013-04-26 17:36:32 +0300

    Fixed some typos

--- url_shortener/tools.py
+++ url_shortener/tools.py
@@ -2,12 +2,12 @@ from hashlib import md5
 
 def make_hash(string):
     """
-    Returns 8-symbols hash of string. Symbols can be [a-z][0-9]
+    Returns 8-symbols hash of string. Symbols can be [a-f][0-9]
     """  
     return md5(string).hexdigest()[:8]
 
 def make_short_url(host, port, url_hash):
     """
-    Returns an url composed from domain name and hash
+    Returns an url composed from host, port and hash
     """
     return "http://%s:%s/%s" % (host, port, url_hash)
