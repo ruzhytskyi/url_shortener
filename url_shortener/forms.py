479b357ae1e30640faa6addcc4bdecfb7205aa48
Oleksandr Ruzhytskyi <oruzhytskyi@cogniance.com>
2013-04-26 16:39:45 +0300

    Fixed bug with url validation

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -1,15 +1,20 @@
 from wtforms import Form, TextField, validators
 from wtforms.validators import ValidationError
-from urllib2 import urlopen, URLError
+from urllib2 import urlopen, URLError, HTTPError
 
 class ShortenerForm(Form):
     no_input_message = 'Paste url and try again'
     full_url = TextField('Full url',
                          [validators.InputRequired(message=no_input_message)])
 
-    def validate_url(form, field):
+    def validate_full_url(form, field):
         url = field.data
         try:
             urlopen(url)
-        except ValueError, URLError:
+        except HTTPError:
+            raise ValidationError('Provided url results in http error, paste another one') 
+        except URLError:
             raise ValidationError('Not a valid url, paste another one')
+        except ValueError:
+            raise ValidationError('Not a valid url, paste another one')
+            
